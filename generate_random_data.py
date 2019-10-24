2a5e5b7297f19b3d52fb297c805c5b0beba4a318
Fahim Tahmid Chowdhury <ftc17@my.fsu.edu>
2019-10-24 11:04:01 -0400

    update project spec and adjust dataset generation code

--- generate_random_data.py
+++ generate_random_data.py
@@ -1,17 +1,23 @@
 import numpy as np
+from pprint import pprint
 
 def generate_class_a_points():
     # method to generate class A points corresponding to +1
+    fsu_lib_id = 33290
+    np.random.seed(fsu_lib_id)
     mean = [-1, -1]
-    covariance_matrix = [[1, 1], [1, 1]]
+    covariance_matrix = [[1, 0], [0, 1]]
     number_of_points = 200
     x, y = np.random.multivariate_normal(mean, covariance_matrix, number_of_points).T
     return zip(x, y)
 
 def generate_class_b_points():
-    # method to generate class A points corresponding to -1
+    # method to generate class B points corresponding to -1
+    fsu_lib_id = 33290
+    np.random.seed(fsu_lib_id)
     mean = [1, 1]
-    covariance_matrix = [[1, 1], [1, 1]]
+    covariance_matrix = [[1, 0], [0, 1]]
     number_of_points = 200
     x, y = np.random.multivariate_normal(mean, covariance_matrix, number_of_points).T
     return zip(x, y)
+
